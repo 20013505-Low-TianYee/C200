9c350a0e81a9ae267f0902ab4bbcff612c6193b8
20013505-Low-TianYee <87228837+20013505-Low-TianYee@users.noreply.github.com>
2022-02-07 23:25:22 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using _4Shapes.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IDBService, DBService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Test/Index");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
