5279a9d204b7b258e07415ff8f8536248daeb9c4
20013505-Low-TianYee <87228837+20013505-Low-TianYee@users.noreply.github.com>
2022-02-07 23:24:19 +0800

    Add files via upload

---
+++ Controllers/ShapesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using _4Shapes.Models;
+using _4Shapes.Services;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+
+namespace _4Shapes.Controllers
+{
+    public class ShapesController : Controller
+    {
+        private readonly IDBService _dbSvc;
+        private readonly IWebHostEnvironment _env;
+
+        public ShapesController(IDBService dbService,
+                              IWebHostEnvironment env)
+        {
+            _dbSvc = dbService;
+            _env = env;
+        }
+        
+        // GET: ShapesController
+        public ActionResult Index()
+        {
+            List<Shapes> lstShapes = _dbSvc.GetList<Shapes>("SELECT * FROM Shapes");
+            return View(lstShapes);
+            
+        }       
+
+     
+    }
+}
---
+++ Models/Shapes.cs
@@ -0,0 +1,11 @@
+﻿namespace _4Shapes.Models
+{
+    public class Shapes
+    {
+        public int Id { get; set; }
+        public string ShapeName { get; set; }
+        public string ProductName { get; set; }
+        public int RestockAmt { get; set; }
+        public int CurrentAmt { get; set; }
+    }
+}
